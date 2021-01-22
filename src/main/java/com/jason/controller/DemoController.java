bd208d53228572561c70deb651579b63f544c0b6
jason <603069092@qq.com>
2021-01-22 20:00:00 +0800

    master第二次提交

--- src/main/java/com/jason/controller/DemoController.java
+++ src/main/java/com/jason/controller/DemoController.java
@@ -1,4 +1,11 @@
 package com.jason.controller;
 
 public class DemoController {
+
+    public void demo(){
+        System.out.println("这是master的demo");
+    }
+    public void test(){
+        System.out.println("这是master的Test");
+    }
 }
